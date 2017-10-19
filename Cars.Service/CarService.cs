0d1952a796a0f56d7a908142a03deacb76314056
Yoana Georgieva <yo.ana.georgieva@abv.bg>
2017-10-19 12:52:38 +0300

    Add car service

---
+++ Cars.Service/CarService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Cars.Models;
+using Cars.Service.Contracts;
+using Cars.Data.Contracts;
+
+namespace Cars.Service
+{
+    public class CarService : ICarService
+    {
+        private readonly IRepository<Car> repository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public CarService(IRepository<Car> repository, IUnitOfWork unitOfWork)
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException(nameof(repository));
+            }
+
+            if (unitOfWork == null)
+            {
+                throw new ArgumentNullException(nameof(unitOfWork));
+            }
+
+            this.repository = repository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public ICollection<Car> GetCarsByOwnerId(Guid? ownerId)
+        {
+            return this.repository
+                .All
+                .Include(c => c.Model)
+                .Include(c => c.Owner)
+                .Where(c => c.OwnerId == ownerId)
+                .ToList();
+        }
+    }
+}
---
+++ Cars.Service/Contracts/ICarService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Cars.Models;
+
+namespace Cars.Service.Contracts
+{
+    public interface ICarService
+    {
+        ICollection<Car> GetCarsByOwnerId(Guid? ownerId);
+    }
+}
