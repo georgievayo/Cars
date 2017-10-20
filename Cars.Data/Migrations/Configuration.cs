b9bb18174231cda6179ccefb59c356d8d9031f7d
Yoana Georgieva <yo.ana.georgieva@abv.bg>
2017-10-20 11:55:06 +0300

    Add seeding data

--- Cars.Data/Migrations/Configuration.cs
+++ Cars.Data/Migrations/Configuration.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using Cars.Models;
+
 namespace Cars.Data.Migrations
 {
     using System.Data.Entity.Migrations;
@@ -9,20 +12,62 @@ namespace Cars.Data.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(Cars.Data.CarsDbContext context)
+        protected override void Seed(CarsDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            this.SeedOwners(context);
+            this.SeedCarModels(context);
+            this.SeedCars(context);
+        }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+        private void SeedOwners(CarsDbContext context)
+        {
+            var owners = new List<Owner>()
+            {
+                new Owner() {Id = 1, FullName = "Pesho Peshov"},
+                new Owner() {Id = 2, FullName = "Gosho Goshov"},
+                new Owner() {Id = 3, FullName = "Tosho Toshov"}
+            };
+
+            foreach (var owner in owners)
+            {
+                context.Owners.Add(owner);
+                context.SaveChanges();
+            }
+        }
+
+        private void SeedCarModels(CarsDbContext context)
+        {
+            var models = new List<CarModel>()
+            {
+                new CarModel() {Id = 1, Description = "Model 1"},
+                new CarModel() {Id = 2, Description = "Model 2"}
+            };
+
+            foreach (var model in models)
+            {
+                context.CarModels.Add(model);
+                context.SaveChanges();
+            }
+        }
+
+        private void SeedCars(CarsDbContext context)
+        {
+            var cars = new List<Car>()
+            {
+                new Car() {Id = 1, OwnerId = 1, ModelId = 1, Description = "Car 1"},
+                new Car() {Id = 2, OwnerId = 3, ModelId = 1, Description = "Car 2"},
+                new Car() {Id = 3, OwnerId = 2, ModelId = 1, Description = "Car 3"},
+                new Car() {Id = 4, OwnerId = 1, ModelId = 2, Description = "Car 4"},
+                new Car() {Id = 5, OwnerId = 2, ModelId = 1, Description = "Car 5"},
+                new Car() {Id = 6, OwnerId = 1, ModelId = 2, Description = "Car 6"},
+
+            };
+
+            foreach (var car in cars)
+            {
+                context.Cars.Add(car);
+                context.SaveChanges();
+            }
         }
     }
 }
